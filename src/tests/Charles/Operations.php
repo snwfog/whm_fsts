05d81e5aa673da53b8d2449bf53af8ff97b2d11e
snwfog <donchoa@gmail.com>
2013-02-11 13:55:58 -0500

    Remove some unused folder for testing git

---
+++ src/tests/Charles/Operations.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: snw
+ * Date: 2013-02-09
+ * Time: 10:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Tests\Charles;
+
+
+class Operations
+{
+
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}45
