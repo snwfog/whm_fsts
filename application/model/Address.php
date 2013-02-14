03a32785401e9fe6b6d097ab32bd3d587694965f
joe-zhou66 <joe.zuzhou@gmail.com>
2013-02-14 18:13:22 -0500

    Add Address and Household entities

---
+++ application/model/Address.php
@@ -0,0 +1,104 @@
+<?php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity @Table(name="address")
+ **/
+ 
+ class Address
+ {
+	
+	/**
+     * @Id @Column(type="integer") @GeneratedValue
+     **/
+    protected $id;
+	/**
+     * @OneToOne(targetEntity="Household", mappedBy="adress")
+     */
+	protected $household;
+	/**
+     * @Column(type="string")
+     **/
+	protected $streetAndNumber;
+	/**
+     * @Column(type="string")
+     **/
+	protected $appt_number;
+	/**
+     * @Column(type="string")
+     **/
+	protected $city;
+	/**
+     * @Column(type="string")
+     **/
+	protected $post_code;
+	/**
+     * @Column(type="string")
+     **/
+	protected $province;
+	
+	public function getStreet()
+	{
+		return $this->streetAndNumber;
+	}
+	public function getHousehold()
+	{
+		return $this->household;
+	}
+	public function getApp_number()
+	{
+		return $this->appt_number;
+	}
+	public function getCity()
+	{
+		return $this->city;
+	}
+	public function getProvince()
+	{
+		return $this->province;
+	}
+	public function getPostCode()
+	{
+		return $this->post_code;
+	}
+	public function setHousehold($household)
+	{
+		$this->household=$household;
+	}
+	public function setStreet($street)
+	{
+		$this->streetAndNumber=$street;
+	}
+	public function setApp_number($appt)
+	{
+		$this->appt_number=$appt;
+	}
+	public function setCity($city)
+	{
+		$this->city=$city;
+	}
+	public function setProvince($province)
+	{
+		$this->province=$province;
+	}
+	public function setPostCode($postCode)
+	{
+		$this->post_code=$postCode;
+	}
+	
+	
+}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
