204747e6ec92b4883f7ea25158ad56d7c5331d8c
Bobby Yit <bobby.yit@gmail.com>
2013-02-14 17:39:35 -0500

    skeleton household controller  test

--- tests/HouseholdControllerTest.php
+++ tests/HouseholdControllerTest.php
@@ -2,7 +2,66 @@
 
 class Household_Controller_Test extends PHPUnit_Framework_TestCase
 {
+    function newHouseholdTest(){
 
+    }
 
+    function registerHouseholdTest(){
+
+    }
+
+    function displayInformationFormTest(){
+
+    }
+
+    function findHouseholdTest($name){
+
+    }
+
+    function displayTest($household_view){
+
+    }
+
+    function removeHouseholdTest($id){
+
+    }
+
+    function findMemberTest($name){
+
+    }
+
+    // function displayTest($memberView){
+
+    // }
+
+    function removeMemberTest($id){
+
+    }
    
+    function editHouseholdTest($id){
+
+    }
+
+    // function displayTest($edithousehold_view){
+
+    // }
+
+    function updateHouseholdTest(){
+
+    }
+
+    function newDependencyTest(){
+
+    }
+
+    // function displayTest($newDependency_view){
+
+    // }
+
+    function addMemberTest(){
+
+    }
+
+
+
 }
