4377da5ba4909391ded847b15af53683b1bc7e35
Bobby Yit <bobby.yit@gmail.com>
2013-02-14 16:00:17 -0500

    test stuff

--- src/tests/BOBBY/CLASSA.php
+++ src/tests/BOBBY/CLASSA.php
@@ -1,23 +1,33 @@
 <?php
 
-class CLASSA{
+require_once ("CLASSB.php");
+	class CLASSA{
+
+		var $foo = "helllooooooo";
+
+		function square($num){
+			return $num * $num;
+		}
 
 
-function square($num){
-	return $num * $num;
-}
+		function makeArray(){
+			$a = array(
+		    "foo" => "bar",
+		    "bar" => "foo",
+			);
+		}
+
+		function makeEmptyArray(){
+			$a = array();
+		}
+
+		function getName(){
+			$class = new CLASSB();
+			$a = $class->getName();
+			return $a;
+
+		}
 
 
-function makeArray($a){
-$a = array(
-    "foo" => "bar",
-    "bar" => "foo",
-);
-
-
-}
-
-
-}
-
+	}
 ?>
