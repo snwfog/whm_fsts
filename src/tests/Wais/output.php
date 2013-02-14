b24025bcb6da19dfe8b54660c2aedebc646f1536
Wais Khedri <khedriwais@hotmail.com>
2013-02-14 16:58:52 -0500

    test try

---
+++ src/tests/Wais/output.php
@@ -0,0 +1,20 @@
+<?php
+class Output
+{
+    //contains the internal data
+    var $data;
+
+    // constructor
+    function __construct($data) {
+        $this->data = $data;
+    }
+
+    public function output()
+    {   
+        print 'test';
+
+    } 
+    
+
+}
+?>
