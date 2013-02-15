173df2ae452aead29874cd89209bff87873a0c48
rz-robsn <rz.robsn@gmail.com>
2013-02-14 20:51:08 -0400

    just reformatted.

--- application/model/Instances.php
+++ application/model/Instances.php
@@ -1,27 +1,29 @@
 <?php
+
 use Doctrine\Common\Collections\ArrayCollection;
-/**
-* @Entity @Table(name="instances")
-**/
-class Instances
-{
 
 /**
-* @Id @Column(type="integer") @GeneratedValue
-**/
-protected $id;
+ * @Entity @Table(name="instances")
+ * */
+class Instances {
 
-/**
-* @ManyToOne(targetEntity="Events") 
-* @JoinColumn(name="event_id", referencedColumnName="id") 
-* 
-*/
-protected $events;
-/**
-* @ManyToOne(targetEntity="Event_Templates") 
-* @JoinColumn(name="template_id", referencedColumnName="id") 
-* 
-*/
-protected $event_templates;
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * */
+    protected $id;
+
+    /**
+     * @ManyToOne(targetEntity="Events") 
+     * @JoinColumn(name="event_id", referencedColumnName="id") 
+     * 
+     */
+    protected $events;
+
+    /**
+     * @ManyToOne(targetEntity="Event_Templates") 
+     * @JoinColumn(name="template_id", referencedColumnName="id") 
+     * 
+     */
+    protected $event_templates;
 
 }
