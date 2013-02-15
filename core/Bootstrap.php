2750611969637f7bfb60e02a6d05cc0ef243c3b8
snwfog <donchoa@gmail.com>
2013-02-15 02:12:00 -0500

    Attach Doctrine manually

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -14,6 +14,10 @@ require_once(SYSPATH . '/Autoload.php');
 // Require the config file for define variables
 require_once(SYSPATH . '/Config.php');
 
-// Load Twig, the PHP templating framework
-require_once('libs/Twig/Autoloader.php');
+// Bootstrap Twig Templating Framework
+require_once('vendor/Twig/Autoloader.php');
 Twig_Autoloader::register();
+
+// Bootstrap Doctrine ORM 2.3.2
+require_once('vendor/Doctrine/Doctrine/ORM/Tools/Setup.php');
+Doctrine\ORM\Tools\Setup::registerAutoloadDirectory("vendor/Doctrine");
