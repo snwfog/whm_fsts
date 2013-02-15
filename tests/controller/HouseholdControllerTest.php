946488b3939e3c43402bbd6e3c200fbb41690c00
snwfog <donchoa@gmail.com>
2013-02-15 02:11:31 -0500

    Attach doctrine manually

---
+++ tests/controller/HouseholdControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+class Household_Controller_Test extends PHPUnit_Framework_TestCase
+{
+    function testNewHousehold()
+    {
+
+    }
+
+    function testRegisterHousehold(){
+
+    }
+
+    function testDisplayInformationForm(){
+
+    }
+
+    function testFindHousehold($name)
+    {
+
+    }
+
+    function testDisplay($household_view, $memberView, $edithousehold_view, $newDependency_view){
+
+    }
+
+    function testRemoveHousehold($id){
+
+    }
+
+    function testFindMember($name){
+
+    }
+
+
+
+    function testRemoveMember($id){
+
+    }
+
+    function testEditHousehold($id){
+
+    }
+
+
+
+    function testUpdateHousehold(){
+
+    }
+
+    function testNewDependency(){
+
+    }
+
+
+    function testAddMember(){
+
+    }
+
+
+
+}
