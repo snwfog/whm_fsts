f20bd418619ed05162ece954e0522a1447682191
nimonikMike <viet_mike.pham@hotmail.com>
2013-02-16 17:47:18 -0600

    test example

--- tests/controller/TestControllerTest.php
+++ tests/controller/TestControllerTest.php
@@ -4,15 +4,13 @@ class Test_Controller_Test extends PHPUnit_Framework_TestCase
 {
     public function testPost()
     {
-        $test_controller = new Test_Controller();
         $_POST = array( "b"=>"batman", "c" => "catwoman" );
-        $observer = $this->getMock('Test_Controller', array('post_call', 'post'));
+
+        $observer = $this->getMock('Test_Controller', array('post_call'));
         $observer->expects($this->once())
                  ->method('post_call')
-                 ->with($this->equalTo("batman"), $this->equalTo("catwoman"));
-        $this->assertEquals("batman", $test_controller->post());
-
-
+                 ->with($this->equalTo("bso"), $this->equalTo("coman"));
+        $observer->post();
     }
  
     public static function tearDownAfterClass()
