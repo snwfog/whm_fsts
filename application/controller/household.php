1c59e6f0a71825e7045318abe286d34bd981a8d5
Wais Khedri <khedriwais@hotmail.com>
2013-02-16 21:42:26 -0500

    updated create household controller

--- application/controller/household.php
+++ application/controller/household.php
@@ -11,22 +11,11 @@ class Household_Controller extends Controller_Core implements IRedirectable_Core
 
     }
 
-    public function get($args = null)
+    public function get()
     {
-        switch ($args) {
-            case 'createHousehold':
-
-                $this->display("household_create_form.twig");
-                break;
-            
-            default:
-                $data = array("first_name" => "wais");
-                $this->display("household_view_form.twig", $data);
-                break;
-        }
-
-		
-			
+       
+        $data = array("first_name" => "wais");
+        $this->display("household_view_form.twig", $data);	
        // $this->display("household_create_form.twig");
     }
 
@@ -46,74 +35,10 @@ class Household_Controller extends Controller_Core implements IRedirectable_Core
 
     }
 
-    public function post()
-    {
-        echo "This is a post";
-
-        if (isset($_POST))
-        {
-            $this->data["form"] = $_POST;
-            print_r($this->data);
-            $this->check_form($this->data["form"]);
-            
-        }
-
-        /*
-
-        $householdModel = new Household();
-        $householdMemberModel  = new HouseholdMember();
-        $addressModel    = new Address();
+   public function post()
+   {
     
-
-        if ($household_instance = $householdModel->create_household($household_member_instance["id"], $address_instance["id"], $this)); 
-        {
-            
-            $household_member_instance = $householdMemberModel ->create_householdMember($_POST["first_name"], $_POST["last_name"], $_POST["work_status"], $_POST["welfare_number"], $_POST["referal"], $_POST["language"], $_POST["martial_status"], $_POST["origin"], $_POST["medicare"]);
-            $address_instance = $addressModel->create_address($_POST["address"], $_POST["city"], $_POST["province"], $_POST["country"], $_POST["postal_code"], $this);
-        
-            return $household_instance;
-
-        } 
-
-        $this->display("household_create_form.twig", $this->data);
-
-        */
-        
-
-
-    }
-
-
-
-     public function check_form($form)
-    {
-        $householdModel = new HouseholdMember();
-        
-        if ($householdModel->exist_attribute("welfare_number", $form["welfare_number"]))
-        {
-            array_push($this->data["errors"], "welfare number Exist!");
-        }
-
-        if ($householdModel->exist_attribute("medicare", $form["medicare"]))
-        {
-            array_push($this->data["errors"], "medicare Exist!");
-        }
-
-
-        if (count($this->data["errors"]) > 0)
-        {
-            $this->display("household_create_form.twig", $this->data);
-        }
-
-        else
-        {
-            $this->post();
-            $this->redirect("index.php?household");
-        }
-
-   
-    }
-
+   }
 
 
 
