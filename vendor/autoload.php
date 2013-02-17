493e1f9a855687aad29cda2eb95789a28ca5eac6
snwfog <donchoa@gmail.com>
2013-02-16 23:49:03 -0500

    Regenerate autoloading from composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit::getLoader();
+return ComposerAutoloaderInit162f582efe579206133befc6bfe2c6e6::getLoader();
