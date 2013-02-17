c6eb10fc092ec826912eb74a75363b0032f20ae4
snwfog <donchoa@gmail.com>
2013-02-17 01:10:54 -0500

    Add use statement for Renderer to load Twig

--- application/whm/Bootstrap.php
+++ application/whm/Bootstrap.php
@@ -3,7 +3,7 @@
 namespace WHM;
 
 /*
- * BOOSTRAP
+ * BOOTSTRAP
  *
  * @description: This file loads files that are not part
  * of any classes.
@@ -16,11 +16,3 @@ require_once(APPPATH . '/whm/Config.php');
 
 // Bootstrap all autoloader functions
 require_once('vendor/autoload.php');
-
-// Bootstrap Twig templating engine
-//require_once('vendor/Twig/Autoloader.php');
-//Twig_Autoloader::register();
-
-// Bootstrap Doctrine ORM 2.3.2
-//require_once('vendor/Doctrine/Doctrine/ORM/Tools/Setup.php');
-//Doctrine\ORM\Tools\Setup::registerAutoloadDirectory("vendor/Doctrine");
