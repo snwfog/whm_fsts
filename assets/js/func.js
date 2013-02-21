e7efe069fd5f80d52e7a2c0ec0fe30f9b1919a08
kquang <k.tran085@gmail.com>
2013-02-21 18:35:57 -0500

    infos fixed

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,7 +1,11 @@
    var selected = null;
+   var done;
 function selectChange(x)
 {
 if(selected != null) selected.className = '';
 selected = x;
 x.className = 'active';
+if(!done)
+if(x = t0 ) x.className ='';
+done = true;
 }
