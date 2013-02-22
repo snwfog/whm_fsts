9cb4020e44fe59debc502efc5be7d63a7fa8f61a
snwfog <donchoa@gmail.com>
2013-02-22 01:37:28 -0500

    Simplify the redirect function

--- application/whm/Controller.php
+++ application/whm/Controller.php
@@ -135,10 +135,7 @@ abstract class Controller
      */
     public function redirect($file = self::REDIRECT_INDEX)
     {
-        $uri = explode("/", rtrim(dirname($_SERVER['PHP_SELF']), '/\\'));
-        $url = '/'. $_SERVER['HTTP_HOST'] . '/'. $uri[1] . '/' . $file;
-
-        header('Location: /' . $url);
+        header('Location: ' . SITE_ROOT . $file);
     }
 
     public function back()
