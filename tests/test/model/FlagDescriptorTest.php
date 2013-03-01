10ae165a6e3a6a65cc8da186ae0a1b6bca75f921
rz-robsn <rz.robsn@gmail.com>
2013-03-01 19:28:05 -0400

    added FlagDescriptorTest.php

---
+++ tests/test/model/FlagDescriptorTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use \PHPUnit_Framework_TestCase;
+use WHM\Model\FlagDescriptor;
+
+class FlagDescriptorTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var FlagDescriptor 
+     */
+    private $flagDescriptor;
+
+    /**
+     * @var FlagDescriptor 
+     */
+    private $flagDescriptor2;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->flagDescriptor = new FlagDescriptor();
+        
+        $this->flagDescriptor2 = new FlagDescriptor();
+        $this->flagDescriptor2->setColor("Red");
+        $this->flagDescriptor2->setMeaning("Warning Flag");
+    }
+
+    public function testGetId()
+    {
+        $this->assertThat(
+            $this->flagDescriptor->getId(),
+            $this->equalTo(null));
+        
+        $this->assertThat(
+            $this->flagDescriptor2->getId(),
+            $this->equalTo(null));
+    }
+
+    public function testSetColor()
+    {
+        $this->flagDescriptor->setColor("Blue");
+        $this->assertThat(
+            PHPUnit_Framework_TestCase::readAttribute($this->flagDescriptor, "color"),
+            $this->equalTo("Blue"));
+    }
+
+    public function testGetColor()
+    {
+        $this->assertThat(
+            $this->flagDescriptor2->getColor(),
+            $this->equalTo("Red"));
+    }
+
+    public function testSetMeaning()
+    {
+        $this->flagDescriptor->setMeaning("A Document Missing flag.");
+        $this->assertThat(
+            PHPUnit_Framework_TestCase::readAttribute($this->flagDescriptor, "meaning"),
+            $this->equalTo("A Document Missing flag."));        
+    }
+
+    public function testGetMeaning()
+    {
+        $this->assertThat(
+            $this->flagDescriptor2->getMeaning(),
+            $this->equalTo("Warning Flag"));        
+    }
+
+}
+
+?>
