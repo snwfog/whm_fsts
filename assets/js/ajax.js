a71de14c6cba519eb2f5d9b44189a9d0982ceea8
snwfog <donchoa@gmail.com>
2013-03-09 19:42:00 -0500

    Tweak manage flag

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -24,33 +24,6 @@ $(function() {
       return $('form[name="flag-create-form"]').submit();
     }
   });
-
-  $('button#create-event-save').click(function() {
-    var clicked, formElement, occurrence;
-    clicked = $("#event-occurrence .active").length > 0;
-    if (clicked) {
-      occurrence = $("#event-occurrence .active").data("value");
-      formElement = document.createElement("input");
-      formElement.setAttribute("name", "occurrence-type");
-      formElement.setAttribute("type", "hidden");
-      formElement.setAttribute("value", occurrence);
-      $('form[name="event-form"]').append(formElement);
-    }
-    return $('form[name="event-form"]').submit();
-  });
-
-
-  $('button#create-template-save').click(function() {
-    var formElement;
-    formElement = document.createElement("input");
-    formElement.setAttribute("name", "is_template");
-    formElement.setAttribute("type", "hidden");
-    formElement.setAttribute("value", 1);
-    $('form[name="event-form"]').append(formElement);
-  
-    return $('form[name="event-form"]').submit();
-  });
-
   $('button.flag-delete-btn').click(function() {
     var $form;
     console.log("trying to delete a flag");
