4a2a1d3417a29a8b0b323a0a4a5715d91187f78f
kquang <k.tran085@gmail.com>
2013-04-05 19:59:00 -0400

    small fixes and changes

--- application/whm/model/ManageAppointment.php
+++ application/whm/model/ManageAppointment.php
@@ -41,7 +41,7 @@ class ManageAppointment
 
     public function getParticipantTimeslot($member_id, $timeslot_id)
     {
-        ManageHousehold::findMember($member_id);
+       $member = ManageHousehold::findMember($member_id);
 
         $timeslot = $this->em->find("WHM\model\Timeslot", (int) $timeslot_id);
         $query = $this->em->createQueryBuilder()
