cb9e8715f374a034fabdb17ea01c376c300e5d0e
Bobby Yit <bobby.yit@gmail.com>
2013-04-07 17:36:44 -0400

    create MOARRR loadfixtures

--- tests/fixtures/EventFixture.php
+++ tests/fixtures/EventFixture.php
@@ -23,12 +23,17 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $twoWeek = new \DateInterval('P2W');
         $threeWeek = new \DateInterval('P3W');
         $oneDay = new \DateInterval('P1D');
+        $twoDays = new \DateInterval('P2D');
         $threeDays = new \DateInterval('P3D');
+        $fourDays = new \DateInterval('P4D');
+        $fiveDays = new \DateInterval('P5D');
+        $sixDays = new \DateInterval('P6D');
         
         $e1 = new Event();
         $e1->setCapacity(90);
         $e1->setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
         $e1->setGroupId(1);
+        $e1->setIsActivated(1);
         $e1->setIsTemplate(false);
         $e1->setName("Food Distribution");
         $e1->setStartDate($now);
@@ -43,8 +48,9 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e2->setCapacity(45);
         $e2->setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
         $e2->setGroupId(2);
+        $e2->setIsActivated(1);
         $e2->setIsTemplate(false);
-        $e2->setName("Mattress Distribution");
+        $e2->setName("Mattress");
         $clone2 = clone $now;
         $e2->setStartDate($clone2->add($oneDay));
         $e2->setStartTime(new \DateTime("9:50"));        
@@ -58,6 +64,7 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e3->setCapacity(100);
         $e3->setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
         $e3->setGroupId(1);
+        $e2->setIsActivated(1);
         $e3->setIsTemplate(false);
         $e3->setName("Food Distribution");
         $clone3 = clone $now;
@@ -74,8 +81,9 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e4->setCapacity(90);
         $e4->setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
         $e4->setGroupId(3);
+        $e4->setIsActivated(1);
         $e4->setIsTemplate(false);
-        $e4->setName("Mattress Distribution");
+        $e4->setName("Mattress");
         $clone4 = clone $now;
         $e4->setStartDate($clone4->add($threeDays));       
         $e4->setStartTime(new \DateTime("18:50"));
@@ -89,6 +97,7 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e5->setCapacity(10);
         $e5->setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
         $e5->setGroupId(4);
+        $e5->setIsActivated(1);
         $e5->setIsTemplate(false);
         $e5->setName("Food Distribution");
         $clone5 = clone $now;
@@ -103,7 +112,8 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e6 = new Event();
         $e6->setCapacity(90);
         $e6->setDescription("Today's special event");  
-        $e6->setGroupId(5);   
+        $e6->setGroupId(5);  
+        $e6->setIsActivated(1); 
         $e6->setIsTemplate(false);
         $e6->setName("Special giveaways");
         $e6->setStartDate($now);
@@ -140,18 +150,15 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e5DateClone = clone $e5->getStartDate();
 
         $e1week2->setStartDate($e1DateClone->add($oneWeek));
+        $this->makeTimeSlot($e1week2);
         $e2week2->setStartDate($e2DateClone->add($oneWeek));
+        $this->makeTimeSlot($e2week2);
         $e3week2->setStartDate($e3DateClone->add($oneWeek));
+        $this->makeTimeSlot($e3week2);
         $e4week2->setStartDate($e4DateClone->add($oneWeek));
+        $this->makeTimeSlot($e4week2);
         $e5week2->setStartDate($e5DateClone->add($oneWeek));
-
-        $slot2 = new Timeslot();
-        $slot2->setCapacity(90);
-        $slot2->setDuration(60);
-        $slot2->setName('Group A');
-
-        $e5week2->addTimeslot($slot2);    
- 
+        $this->makeTimeSlot($e5week2);
 
         $manager->persist($e1week2);
         $manager->persist($e2week2);
@@ -159,8 +166,6 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $manager->persist($e4week2);
         $manager->persist($e5week2);
 
-
-       
         // Adding similar events for two week    
         $e1week2 = clone $e1;
         $e2week2 = clone $e2;
@@ -175,25 +180,21 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e5DateClone = clone $e5->getStartDate();
 
         $e1week2->setStartDate($e1DateClone->add($twoWeek));
+        $this->makeTimeSlot($e1week2);
         $e2week2->setStartDate($e2DateClone->add($twoWeek));
+        $this->makeTimeSlot($e2week2);
         $e3week2->setStartDate($e3DateClone->add($twoWeek));
+        $this->makeTimeSlot($e3week2);
         $e4week2->setStartDate($e4DateClone->add($twoWeek));
+        $this->makeTimeSlot($e4week2);
         $e5week2->setStartDate($e5DateClone->add($twoWeek));
-
-        $slot2 = new Timeslot();
-        $slot2->setCapacity(90);
-        $slot2->setDuration(60);
-        $slot2->setName('Group A');
-
-        $e5week2->addTimeslot($slot2);     
+        $this->makeTimeSlot($e5week2);    
         
         $manager->persist($e1week2);
         $manager->persist($e2week2);
         $manager->persist($e3week2);
         $manager->persist($e4week2);
         $manager->persist($e5week2);
-
-
 
         // Adding similar events for three week later   
         $e1week2 = clone $e1;
@@ -209,10 +210,15 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $e5DateClone = clone $e5->getStartDate();
 
         $e1week2->setStartDate($e1DateClone->add($threeWeek));
+        $this->makeTimeSlot($e1week2);
         $e2week2->setStartDate($e2DateClone->add($threeWeek));
+        $this->makeTimeSlot($e2week2);
         $e3week2->setStartDate($e3DateClone->add($threeWeek));
+        $this->makeTimeSlot($e3week2);
         $e4week2->setStartDate($e4DateClone->add($threeWeek));
+        $this->makeTimeSlot($e4week2);
         $e5week2->setStartDate($e5DateClone->add($threeWeek));
+        $this->makeTimeSlot($e5week2);
 
         $slot2 = new Timeslot();
         $slot2->setCapacity(90);
@@ -227,12 +233,67 @@ class EventFixture extends AbstractFixture implements DependentFixtureInterface
         $manager->persist($e4week2);
         $manager->persist($e5week2);
 
+        $this->createEvents($manager, $oneDay);
+        $this->createEvents($manager, $twoDays);
+        $this->createEvents($manager, $threeDays);
+        $this->createEvents($manager, $fourDays);
+        $this->createEvents($manager, $fiveDays);
+        $this->createEvents($manager, $sixDays);
 
+        for($j = 8; $j <= 200; $j++)
+        {
+            $stringDays = 'P'.$j.'D';
+            $days = new \DateInterval($stringDays);
+            
+            $this->createEvents($manager, $days);
+        }
 
         $manager->flush();
 
         $this->addReference('timeslot1', $slot1); 
         $this->addReference('timeslot2', $slot6);       
+    }
+
+    private function makeTimeSlot($event)
+    {
+        $capacity = ceil(rand(30, 100) / 10) * 10;
+        $time = array("30", "60");
+        $duration = $time[rand(0,1)];
+        $numberOfSlots = rand(4,6);
+
+        for($i = 1; $i <= $numberOfSlots; $i++)
+        {   
+            $slot = new Timeslot();
+            $slot->setCapacity($capacity);
+            $slot->setDuration($duration);
+            $slot->setName('Timeslot');
+            $event->addTimeslot($slot);
+            $slot = null;
+        }
+    }
+
+    private function createEvents($manager, $day)
+    {
+        $now = new \DateTime();
+        $eventsToCreate = rand(2, 4);
+        $name = array("Mattress", "Food Bank", "Christmas", "Income Tax", "BackToSchool");
+
+        for($i = 1; $i <= $eventsToCreate ; $i++)
+        {
+            $event = new Event();
+            $event->setCapacity(45);
+            $event->setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+            // $event->setGroupId(2);
+            $event->setIsActivated(1);
+            $event->setIsTemplate(false);
+            $event->setName($name[rand(0,4)]);
+            $clone = clone $now;
+            $event->setStartDate($clone->add($day));
+            $event->setStartTime(new \DateTime("13:00"));
+            $this->makeTimeSlot($event);
+
+            $manager->persist($event);
+        }
     }
 
 }
