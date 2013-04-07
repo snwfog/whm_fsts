e5932ac1d1665955ebd7adea348d94f8e4cc86d6
joe-zhou66 <joe.zuzhou@gmail.com>
2013-04-07 18:44:41 -0400

    update ManageAppointmentTest.php file

--- tests/test/model/ManageAppointmentTest.php
+++ tests/test/model/ManageAppointmentTest.php
@@ -8,22 +8,39 @@ use WHM\Model\ManageAppointment;
 
 class ManageAppointmentTest extends PHPUnit_Framework_TestCase
 {
+    private $em;
+    private $manageAppointment;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->em = Application::em();
+        $this->manageAppointment = new ManageAppointment();
+    }
     public function testAddAppointment()
     {
-        $manageAppointment = new ManageAppointment;
-        $member = $manageAppointment->addAppointment(1, 1);
-        $this->assertEquals("VIERA",$member->getFirstName());
+        $member = $this->manageAppointment->addAppointment(1, 2);
+        $participantTimeslot = $this->em->createQuery('SELECT p FROM WHM\Model\ParticipantsTimeslots p 
+                                         WHERE p.household_member= 1 AND p.timeslot= 2')->getResult();
+    
+        $this->assertNotNull($participantTimeslot);
     }
+    public function testGetAppointment()
+    {
+        $pt = $this->manageAppointment->getParticipantTimeslot(1,2);
+        $this->assertNotNull($pt);
+    }
+
+
     public function testDeleteAppointment()
     {
-        $manageAppointment = new ManageAppointment;
-        $manageAppointment->deleteAppointment(1, 1);
-        $em = Application::em();
-        $participantTimeslot = $em->createQuery('SELECT p FROM WHM\Model\ParticipantsTimeslots p 
-                                         WHERE p.household_member= 1 AND p.timeslot= 1')->getResult();
-    
-        $this->assertEquals(0,  sizeof($participantTimeslot));
+        $this->manageAppointment->deleteAppointment(1, 2);
+        //$this->assertFalse($this->manageAppointment->getParticipantTimeslot(1,2));
+//        $pt = $em->createQuery('SELECT COUNT(p.id) FROM WHM\Model\ParticipantsTimeslots p')->getResult();
+//    
+//        $this->assertEquals(0,  $pt);
     }
+    
 }
 
 ?>
